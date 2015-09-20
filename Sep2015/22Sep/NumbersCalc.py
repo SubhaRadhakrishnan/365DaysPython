7d9ee09a63b020790efd633f74d50575fe6d8f64
SubhaRadhakrishnan <subha.radhakrishnan@gmail.com>
2015-09-20 21:56:47 +0530

    21th Sept 2015 - GoogleCodeJam- ReverseWord

---
+++ Sep2015/22Sep/NumbersCalc.py
@@ -0,0 +1,26 @@
+__Date__ = '9/22/2015'
+import math
+from fractions import Fraction
+import cmath
+from decimal import Decimal
+def numberCalc(inNum):
+    const=Decimal(3+Decimal(math.sqrt(5)))
+    powNum=Decimal(math.pow(const,inNum))
+    resultPowFloor = math.trunc(powNum)
+    resultStr = str('{0:03d}'.format(resultPowFloor))
+    strLength = len(resultStr)
+    return  resultStr[strLength-3:strLength] + '\n'
+
+from sys import argv
+script, inFilename = argv
+print( "File Name is",inFilename)
+outFileName = inFilename.rstrip('in')+'out'
+writeFile = open(outFileName,'w')
+count=0
+print(numberCalc(19))
+for line in open(inFilename):
+    if(count !=0):
+        numInput = int(line.rstrip('\n'))
+        print(numInput,"Case #"+ str(count) + ': ' + numberCalc(numInput))
+        writeFile.write("Case #"+ str(count) + ': ' + numberCalc(numInput))
+    count+=1
