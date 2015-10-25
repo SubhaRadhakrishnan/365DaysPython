9282d48496e92fb6e94481bf45e7261953a4ae57
SubhaRadhakrishnan <subha.radhakrishnan@gmail.com>
2015-10-25 11:45:15 +0530

    26th Sept 2015 - From "http://www.practicepython.org/"***************** 4)Create a program that asks the user for a number and then prints out a list of all the divisors of that number.  (If you don't know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26/13 has no remainder.)

---
+++ Sep2015/26Sep/NumDivisors.py
@@ -0,0 +1,15 @@
+__Date__ = '9/26/2015'
+"""From "http://www.practicepython.org/"*****************
+4)Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+ (If you don't know what a divisor is, it is a number that divides evenly into another number.
+For example, 13 is a divisor of 26 because 26/13 has no remainder.)
+"""
+number=int(input("Please enter a number greater than 2:"))
+possDivisors = list(range(2,(number//2)+1))
+divisorList = []
+for x in possDivisors:
+    if (number %x)==0:
+        divisorList.append(x)
+print(divisorList)
+
+
