8778ffb7fc353695df6dc6ccf5be3c9d600073bf
SubhaRadhakrishnan <subha.radhakrishnan@gmail.com>
2015-10-31 20:33:00 +0530

    -

--- Oct2015/1Oct/GuessNumber.py
+++ Oct2015/1Oct/GuessNumber.py
@@ -5,6 +5,28 @@ Ask the user to guess the number, then tell them whether they guessed too low, t
 (Hint: remember to use the user input lessons from the very first exercise)
 
 Extras:
-•Keep the game going until the user types “exit”
-•Keep track of how many guesses the user has taken, and when the game ends, print this out.
+Keep the game going until the user types 'exit'
+Keep track of how many guesses the user has taken, and when the game ends, print this out.
 """
+
+import random
+totalcount=0
+succ=0
+misses=0
+while True:
+    randNum=random.randint(2,40)
+    strin = input("Guess a number or type E(xit)")
+    if strin=="E":
+        print("Total guesses:" + str(totalcount) + ", success:"+str(succ)+",misses:"+str(misses))
+        print("Bbye!!")
+        break
+    elif (int(strin)==randNum):
+        print("You Guessed right!!")
+        succ+=1
+    else:
+        print("Sorry, wrong guess. Correct number was "+str(randNum))
+        misses+=1
+    totalcount+=1
+
+
+
