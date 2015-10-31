2a1072129c62644497fd7ee16f16b34172339657
SubhaRadhakrishnan <subha.radhakrishnan@gmail.com>
2015-10-31 21:23:31 +0530

    6Oct2015 From "http://www.practicepython.org/"***************** 14)Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.

--- Oct2015/6Oct/remDupes.py
+++ Oct2015/6Oct/remDupes.py
@@ -2,12 +2,25 @@ __Date__ = '10/31/2015'
 """From "http://www.practicepython.org/"*****************
 14)Write a program (function!) that takes a list and returns a new list that contains all the elements of the
 first list minus all the duplicates.
+Extras:
+Write two different functions to do this - one using a loop and constructing a list, and another using sets.
 """
 
 import random
 #firstArr = random.sample(range(50),random.randint(1,40))
 firstArr=[1,2,3,4,3,2,1]
 print(firstArr)
+
 def remDupes(inArr):
     return list(set(inArr))
+
+
+def remDupesLoops(loopArr):
+    resultArr=[]
+    for i in loopArr:
+        if i not in resultArr:
+            resultArr.append(i)
+    return resultArr
+
 print(remDupes(firstArr))
+print(remDupesLoops(firstArr))
