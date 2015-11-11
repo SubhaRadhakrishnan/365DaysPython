4a066494895cf86fb488dad904648e759c29fa62
SubhaRadhakrishnan <subha.radhakrishnan@gmail.com>
2015-11-11 06:22:02 +0530

    8 Oct 2015
    To do the countdown

--- Oct2015/8Oct/DateDiff.py
+++ Oct2015/8Oct/DateDiff.py
@@ -3,4 +3,4 @@ __Date__ = '11/1/2015'
 To do the countdown :)
 """
 import datetime
-print(str((datetime.date(2016, 7, 1) - datetime.date.today()).days) +" Days to go!!! :)")
+print(str(datetime.date(2016, 7, 1) - datetime.date.today()) +" Days to go!!! :)")
